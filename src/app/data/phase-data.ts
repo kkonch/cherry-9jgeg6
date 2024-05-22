32b478bdf5ca3928009ebadd89b6046a3f63d2f3
Queensley Lim <queensleylim@dhcp-172-31-254-149.mobile.uci.edu>
2024-05-21 19:09:58 -0700

    Added few visuals for tab3 and began layout for data and service

---
+++ src/app/data/phase-data.ts
@@ -0,0 +1,41 @@
+export class PhaseData {
+    public static phase_type = [undefined,
+        'Menstrual Phase',
+        'Ovulation Phase',
+        'Follicular Phase',
+        'Luteal Phase'
+    ];
+
+    public static menstrual = [
+        'It\'s common to feel more sensitive or irritable during this time. Engaging in stress-reducing activities like meditation can help manage mood fluctuations.',
+        'Some individuals may feel fatigued during their menstrual phase, others may find exercise beneficial for managing symptoms like cramps and bloating.',
+        'Foods rich in antioxidants, such as berries, dark chocolate, nuts, and colorful fruits and vegetables, can be incorporated into your diet to support overall health and well-being during menstruation. ',
+        'Hormonal fluctuations during the menstrual phase can impact energy levels, leading to feelings of fatigue or lethargy. Gentle activities like stretching or short walks can also boost energy without adding additional strain. The best exercise during your period is one that feels good to you!'
+    ];
+
+    public static ovulation = [
+        'Ovulation is often accompanied by an increase in estrogen levels, which can contribute to feelings of positivity, confidence, and vitality.',
+        'Ovulation leads to increased energy levels and improved muscle recovery. Strength training exercises, such as lifting weights or using resistance bands, can help capitalize on this phase by building lean muscle mass and improving overall strength.',
+        'Incorporating nutrient-rich foods like leafy greens, whole grains, nuts, seeds, and lean meats can provide the essential vitamins and minerals your body needs during this phase.',
+        'To maintain your energy levels and support your body\'s needs, prioritize staying hydrated, getting enough sleep, and consuming regular, balanced meals.'
+    ];
+
+    public static follicular = [
+        'Engaging in activities that promote mental well-being, such as yoga, meditation, or creative hobbies, can help harness these positive emotions and support overall mood stability during this phase.',
+        'Focus on activities that you enjoy and that challenge different muscle groups, such as strength training, cardio workouts, yoga, or dance classes.',
+        'Aim to include a variety of nutrient-dense foods in your meals, such as fruits, vegetables, whole grains, lean proteins, and healthy fats.',
+        'The follicular phase is typically associated with increasing energy levels and a sense of vitality as estrogen levels rise. Prioritize staying hydrated, getting enough sleep, and fueling your body with balanced meals. '
+    ];
+
+    public static luteal = [
+        'You may be feeling a little more depressed / anxious in this phase.',
+        'Calming yoga may serve better than extraneous exercises in this phase.',
+        'Complex carbohydrates and high-fiber foods, such as cruciferous vegetables, leafy greens and sweet potatoes, will help curb hunger. Dark chocolate, fruit, nuts and seeds are good for any cravings during this phase.',
+        'It’s normal to feel sluggish, fatigued, and drops in energy as well increased cravings for carbohydrates and sweets, which can impact energy levels and mood. Keep yourself hydrated, sleep, and practice stress management techniques to keep yourself regulated!'
+    ];
+
+    constructor() {
+        
+    }
+
+}
---
+++ src/app/services/phase-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { PhaseData } from "../data/phase-data";
+
+export class PhaseService {
+
+
+    constructor() {
+        this.addDefaultData();
+    }
+
+
+    private addDefaultData() {
+        // this.log
+    }
+
+}
