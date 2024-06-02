61accf0a6f9de2e6b7cf4096f4c77af8ea1fec35
Queensley Lim <queensleylim@Queensleys-Air.home>
2024-06-02 15:36:22 -0700

    Temporarily commented dounut chart, added in more containers for date and donut chart

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,46 +6,47 @@ import { Chart, registerables } from 'chart.js';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements AfterViewInit {
+//export class Tab1Page implements AfterViewInit {
 
-  @ViewChild('doughnutCanvas') doughnutCanvas!: ElementRef<HTMLCanvasElement>;
-  doughnutChart: any;
+export class Tab1Page {
+  // @ViewChild('doughnutCanvas') doughnutCanvas!: ElementRef<HTMLCanvasElement>;
+  // doughnutChart: any;
 
   constructor() {
-    Chart.register(...registerables);
+    // Chart.register(...registerables);
   }
 
-  ngAfterViewInit() {
-    this.doughnutChartMethod();
-  }
+  // ngAfterViewInit() {
+  //   this.doughnutChartMethod();
+  // }
 
-  doughnutChartMethod() {
-    const ctx = this.doughnutCanvas.nativeElement.getContext('2d');
-    if (ctx) {
-      this.doughnutChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-          labels: ['Menstruation', 'Ovulation', 'Follicular', 'Luteal'],
-          datasets: [{
-            label: '# of days',
-            data: [7, 7, 10, 4],
-            backgroundColor: [
-              '#84012A',
-              '#4F5172',
-              '#989FC7',
-              '#282749',
-            ],
-            hoverBackgroundColor: [
-              '#4a0017',
-              '#33344a',
-              '#737893',
-              '#10101d',
-            ]
-          }]
-        }
-      });
-    }
-  }
+  // doughnutChartMethod() {
+  //   const ctx = this.doughnutCanvas.nativeElement.getContext('2d');
+  //   if (ctx) {
+  //     this.doughnutChart = new Chart(ctx, {
+  //       type: 'doughnut',
+  //       data: {
+  //         labels: ['Menstruation', 'Ovulation', 'Follicular', 'Luteal'],
+  //         datasets: [{
+  //           label: '# of days',
+  //           data: [7, 7, 10, 4],
+  //           backgroundColor: [
+  //             '#84012A',
+  //             '#4F5172',
+  //             '#989FC7',
+  //             '#282749',
+  //           ],
+  //           hoverBackgroundColor: [
+  //             '#4a0017',
+  //             '#33344a',
+  //             '#737893',
+  //             '#10101d',
+  //           ]
+  //         }]
+  //       }
+  //     });
+  //   }
+  // }
 }
 
 
