2f12a5c88f846837a4ce6566c924a2943a728f61
Queensley Lim <queensleylim@dhcp-v054-221.mobile.uci.edu>
2024-06-04 17:11:49 -0700

    Added in second page for tab

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  items: any  = [];
+
+  ngOnInit(): void {
+
+  }
 
   constructor() {}
+
+  sectionChange(num: number) {
+    console.log(1);
+    const calendarContainer = document.getElementById("calendar-container");
+    const mainSymptomsContainer = document.getElementById("main-symptoms-container");
+
+    if (calendarContainer && mainSymptomsContainer) {
+      switch(num) {
+        case 1:
+          calendarContainer.style.display = "none";
+          mainSymptomsContainer.style.display = "flex";
+          break;
+        case 2:
+          console.log(2);
+
+          calendarContainer.style.display = "flex";
+          mainSymptomsContainer.style.display = "none";
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  handleCancel() {
+    console.log(3);
+  }
 
 }
